4e92ae8061fc57eec35134d442fd4c2af8866013
ImranTheMan <ijason2002@gmail.com>
2024-10-24 15:34:01 +0800

    Update Index.py

--- index.py
+++ index.py
@@ -8,18 +8,21 @@ def get_ip_info():
         ip_info = response.json()
 
         # Extract relevant information
-        public_ip = ip_info.get("ip")
-        city = ip_info.get("city")
-        region = ip_info.get("region")
-        country = ip_info.get("country")
-        isp = ip_info.get("org")
-        asn = ip_info.get("asn")
+        public_ip = ip_info.get("ip","N/A")
+        city = ip_info.get("city","N/A")
+        region = ip_info.get("region","N/A")
+        country = ip_info.get("country","N/A")
+        isp = ip_info.get("org","N/A")
+        asn = ip_info.get("asn","N/A")
+        lat = ip_info.get("latitude", "N/A")
+        lon = ip_info.get("longitude", "N/A")
 
         # Display the information
         print(f"Public IP: {public_ip}")
         print(f"Location: {city}, {region}, {country}")
         print(f"ISP: {isp}")
         print(f"ASN: {asn}")
+        print(f"Latitude: {lat}, Longitude: {lon}")
 
     except requests.RequestException as e:
         print(f"Error retrieving IP information: {e}")
